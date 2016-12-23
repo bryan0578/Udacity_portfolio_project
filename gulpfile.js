83f6be419c4634cf03042895876d1b83631fb135
Bryan Cash <bcash0578@gmail.com>
2016-12-23 01:15:58 -0500

    Gulp CSS Task
    - Gulp require precss
    - Gulp require postcss
    - Gulp require CSSnano
    - Gulp require Autoprefixer
    - Created CSS task
    - Added CSS task to watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,23 @@ var gulp = require('gulp'),
 	webserver = require('gulp-webserver'),
 	imagemin = require('gulp-imagemin'),
 	minify = require('gulp-minify'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	precss = require('precss'),
+	cssnano = require('cssnano'),
 
 	source = 'development/',
 	dest = 'production/';
+
+gulp.task('css', function(){
+	gulp.src(source + '**/*.css')
+		.pipe(postcss([
+			precss(),
+			autoprefixer(),
+			cssnano()
+			]))
+		.pipe(gulp.dest(dest));
+});
 
 gulp.task('javascript', function(){
 	gulp.src(source + 'js/*.js')
@@ -27,8 +41,9 @@ gulp.src(dest)
 });
 
 gulp.task('watch', function(){
+	gulp.watch(source +'**/*.css', ['css']);
 	gulp.watch(source +'js/**/*.js', ['javascript']);
 	gulp.watch(source + 'images/**/*.{jpg, JPG, png}');
 });
 
-gulp.task('default', ['webserver', 'compress-images', 'watch', 'javascript']);
+gulp.task('default', ['webserver', 'compress-images', 'watch', 'javascript', 'css']);
