6b411189ea667d9884745e17530657f3c79cc025
Bryan Cash <bcash0578@gmail.com>
2016-12-23 00:13:33 -0500

    Compressing Images
    - Added gulp task imagemin to compress images

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
 	webserver = require('gulp-webserver'),
+	imagemin = require('gulp-imagemin'),
 
 	source = 'development/',
 	dest = 'production/';
+
+gulp.task('compress-images', function(){
+	gulp.src(source + 'images/**/*.{jpg, JPG, png}')
+		.pipe(imagemin({ progressive: true, optimizationlevel: 7}))
+		.pipe(gulp.dest(dest + 'images'));
+});
 
 gulp.task('webserver', function(){
 gulp.src(dest)
@@ -12,4 +19,4 @@ gulp.src(dest)
 	}));
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('default', ['webserver', 'compress-images']);
